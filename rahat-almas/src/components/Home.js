f83f7857b545527202468c81fad527fe991b8a54
rahatalmas <rahat.almas5599@gmail.com>
2023-06-16 20:22:13 +0600

    template-update

--- rahat-almas/src/components/Home.js
+++ rahat-almas/src/components/Home.js
@@ -109,9 +109,9 @@ const Home = () =>{
                 </div>
           </div>
 
-          <div>
-            Footer
-          </div>
+          <footer>
+            rahatalmas
+          </footer>
      
        </div>
     );
