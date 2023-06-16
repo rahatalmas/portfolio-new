6f23333912cb5d17ad9d88ec8882447985a324ba
= <=>
2023-06-16 06:00:30 +0000

    temp

--- rahat-almas/src/components/Home.js
+++ rahat-almas/src/components/Home.js
@@ -4,13 +4,34 @@ import './Home.css';
 const Home = () =>{
     return(
        <div className='component-container'>
-          <div className='intro-image-container'>
-               <img alt='rahat-almas' src='./images/vector2.png' style={{width:"500px",}}/>
+
+          <div className='home'>
+                <div className='intro-image-container'>
+                    <img alt='rahat-almas' src='./images/vector2.png' style={{width:"500px",}}/>
+                </div>
+                <div className='intro-text-container'>
+                    <h1>Hi, I'm Rahat Almas</h1>
+                    <p>I'm a Full Stack Developer</p>
+                </div>
           </div>
-          <div className='intro-text-container'>
-               <h1>Hi, I'm Rahat Almas</h1>
-               <p>I'm a Full Stack Developer</p>
+
+
+          <div className='about'>
+                <div className='language-skills'>
+                    <div>
+                        language section
+                    </div>
+                    <div>
+                        Skill section
+                    </div>
+                </div>
+                <div className='projects'>
+                    <div>
+                        project card
+                    </div>
+                </div>
           </div>
+     
        </div>
     );
 }
